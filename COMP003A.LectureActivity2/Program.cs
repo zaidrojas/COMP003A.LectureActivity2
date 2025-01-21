3a112c7b6647291192cfa4db245d3178f1660bf7
zaidrojas <zaidnemesis83@outlook.com>
2025-01-21 12:47:37 -0800

    Add project files.

---
+++ COMP003A.LectureActivity2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.LectureActivity2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
